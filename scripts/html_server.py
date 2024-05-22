8b80bcd4c19df1355f3a6c16c7bb430b1846ba57
Joao <joaoweslley.souza55@gmail.com>
2024-05-22 13:08:44 -0300

    Ajustado para funcionar com apenas um servidor

--- scripts/html_server.py
+++ scripts/html_server.py
@@ -1,13 +1,32 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import sqlite3
+import re
+from os import remove
+import socket
+import threading
 
+# configurações do servidor
+SERVER_IP = input('Insira o endereço IP do servidor... ')
+PORT = 59000
+clients = []
+
+
+
+# configurações do Flask
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/api/devices')
 def device_status():
-    conn = sqlite3.connect('database/main_database.db')
+
+    try:
+        conn = sqlite3.connect('../database/main_database.db')
+    except Exception as e:
+        print('Erro ocorrido! Você tentou acessar a pasta "scripts/"?')
+        raise e
+
+
     cursor = conn.cursor()
 
     cursor.execute("SELECT * FROM maquina_cadastro")
@@ -20,5 +39,170 @@ def device_status():
 
     return jsonify([cadastro,status],[])
 
+
+
+# funções de administração do banco
+def generate_database():
+
+    # remove os dados do banco, zerando ele
+    try:
+        remove('../database/main_database.db')
+    except:
+        print('Database empty')
+
+    conn = sqlite3.connect('../database/main_database.db')
+
+    cursor = conn.cursor()
+
+    cursor.execute('''
+CREATE TABLE IF NOT EXISTS maquina_cadastro (
+                id INTEGER PRIMARY KEY,
+                ip VARCHAR(15) DEFAULT NULL,
+                alias VARCHAR(255),
+                situacao VARCHAR(1) DEFAULT I
+)''')
+
+
+    cursor.execute('''
+CREATE TABLE IF NOT EXISTS maquina_status(
+                   id INTEGER PRIMARY KEY,
+                   cpu_usage_geral REAL DEFAULT 0,
+                   cpu_usage_detail VARCHAR(255) DEFAULT 0,
+                   memory_usage REAL DEFAULT 0,
+                   FOREIGN KEY (id) REFERENCES maquina_cadastro(id)
+)''')
+
+    conn.commit()
+
+    conn.close()
+
+
+def cadastrar_maquina(ip_add:'str',alias:'str'):
+
+    try:
+        conn = sqlite3.connect('../database/main_database.db')
+    except:
+        conn = sqlite3.connect('database/main_database.db')
+    
+    cursor = conn.cursor()
+
+    try:
+        conn.execute('BEGIN TRANSACTION')
+
+        cursor.execute('INSERT INTO maquina_cadastro (ip, alias, situacao) VALUES (?, ?, ?)', (ip_add, alias, 'A'))
+        id = cursor.lastrowid
+
+        cursor.execute('INSERT INTO maquina_status (id) VALUES (?)', (id,))
+
+        conn.execute('COMMIT')
+
+    except Exception as e:
+        conn.execute('ROLLBACK')
+        raise e
+    
+    finally:
+        conn.close()
+
+
+def atualizar_status(id:'int',cpu_t:'float',cpu_d:'str',mem:'float'):
+
+    try:
+        conn = sqlite3.connect('../database/main_database.db')
+    except Exception as e:
+        print('Erro ocorrido! Você tentou acessar a pasta "scripts/"?')
+        raise e
+    
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM maquina_cadastro WHERE id = ? AND situacao = 'A'",(id,))
+
+    if cursor.fetchall() != []:
+        cursor.execute("UPDATE maquina_status SET cpu_usage_geral = ?, cpu_usage_detail = ?, memory_usage = ? WHERE id = ?",
+                       (cpu_t,cpu_d,mem,id))
+        conn.commit()
+    else:
+        print(f'A máquina de id {id} está inativa!')
+    
+    conn.close()
+
+def desativar_maquina(id:'int'):
+    
+    try:
+        conn = sqlite3.connect('../database/main_database.db')
+    except Exception as e:
+        print('Erro ocorrido! Você tentou acessar a pasta "scripts/"?')
+        raise e
+
+    cursor = conn.cursor()
+
+    cursor.execute('UPDATE maquina_cadastro SET situacao = ? WHERE id = ?',
+                   ('I',id))
+    
+    conn.commit()
+    conn.close()
+
+
+
+# funções do servidor
+def receber_do_cliente(message:'str',client):
+
+    index = clients.index(client)+1
+
+    cpu_t = float(re.split(r'\|',message)[0])
+    cpu_d = str(re.split(r'\|',message)[1])
+    mem = float(re.split(r'\|',message)[2])
+
+    atualizar_status(index,cpu_t,cpu_d,mem)
+
+
+def gerir_cliente(client):
+
+    while True:
+        try:
+            message = str(client.recv(1024).decode('utf-8'))
+            if len(message) == 0:
+                index = clients.index(client)+1
+                client.close()
+
+                desativar_maquina(index)
+
+                print(f'{client} desconectou...')
+
+                break
+            else:
+                receber_do_cliente(message,client)
+        
+        except:
+            index = clients.index(client)+1
+            client.close()
+            desativar_maquina(index)
+            break
+
+
+def socket_server():
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((SERVER_IP,PORT))
+    server.listen()
+
+    while True:
+        client, address = server.accept()
+        print(f'Conexão estabelecida com {str(address[0])}')
+        client.send('alias?'.encode('utf-8'))
+        alias = client.recv(1024).decode('utf-8')
+        clients.append(client)
+        
+        cadastrar_maquina(str(address[0]),alias)
+
+        client.send('Conectado!'.encode('utf-8'))
+        thread = threading.Thread(target=gerir_cliente, args=(client,))
+        thread.start()
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=59001)
+    generate_database()
+    
+    flask_thread = threading.Thread(target=app.run,kwargs={'port':59001})
+    flask_thread.start()
+
+    socket_server()
