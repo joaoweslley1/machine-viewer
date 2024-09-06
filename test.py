d2d41d99d7042dcbc8f01294af75d056ac322c70
João Weslley <joaoweslley.ouza55@gmail.com>
2024-09-06 15:31:18 -0300

    Alterando organização do projeto

--- test.py
+++
@@ -1,5 +0,0 @@
-
-configs_path = '../../configs/ip_addrs'
-database_path = '../../database/database.db'
-print('/'.join(configs_path.split('/')[:3]) + '/')
-print('/'.join(database_path.split('/')[:3]) + '/')
